d42a8aa78797e7cfa57fd12223074c3198958bf0
HasanM789 <doniepreo@gmail.com>
2026-07-13 11:08:26 +0300

    Fix test cart to use correct session

--- test_cart.php
+++ test_cart.php
@@ -1,9 +1,27 @@
 <?php
-session_start();
+require_once('db_config.php');
 
-// Add a test item
-$_SESSION['cart'][17] = 2;
+// Add a test item - uses the SAME session as the rest of your site
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+}
 
-echo "✅ Test item added!<br>";
-echo "<a href='cart.php'>View Cart</a>";
+// Add product ID 17 (Classic White Tee)
+if (isset($_SESSION['cart'][17])) {
+    $_SESSION['cart'][17]++;
+} else {
+    $_SESSION['cart'][17] = 1;
+}
+
+// Also add product 18 (Relaxed Fit Denim)
+if (isset($_SESSION['cart'][18])) {
+    $_SESSION['cart'][18]++;
+} else {
+    $_SESSION['cart'][18] = 1;
+}
+
+echo "✅ Test items added to cart!<br>";
+echo "Product 17 (Classic White Tee) x " . $_SESSION['cart'][17] . "<br>";
+echo "Product 18 (Relaxed Fit Denim) x " . $_SESSION['cart'][18] . "<br>";
+echo "<br><a href='cart.php'>View Cart</a>";
 ?>
